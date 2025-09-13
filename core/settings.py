c604c35e6b312b0470dabc7c0a0e0043047b1164
Ammar <ch.ammar.tech@example.com>
2025-09-14 02:58:03 +0500

    Add render deploy config: Procfile, static settings

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 PRE_APPS = [
@@ -142,15 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Add this to let Django find static files from your apps and allauth
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
+STATIC_URL = '/static/'
 # This is for deployment, but good to have
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
