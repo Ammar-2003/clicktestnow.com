bcbc4342f60c2fc4b2245e747f93e47368440625
Ammar <ch.ammar.tech@example.com>
2025-09-14 03:15:34 +0500

    changed base_dir

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ import os
 SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
