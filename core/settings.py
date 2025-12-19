1bcc517615ada80b78228c4b1a2437baef77da1f
Ammar <ch.ammar.tech@example.com>
2025-12-20 01:02:23 +0500

    debug is True for testing Purpose

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ load_dotenv()  # loads .env into os.environ as strings
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'clicktestnow.com']
 # settings.py (development only!)
