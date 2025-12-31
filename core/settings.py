6f0a367ff0fb43098265c1e5f1751ea68800cc00
Ammar <ch.ammar.tech@example.com>
2025-12-31 05:06:36 +0500

    pages are mobile optimized

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ load_dotenv()  # loads .env into os.environ as strings
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'clicktestnow.com', 'ilovemouse-com.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'clicktestnow.com', 'ilovemouse-com.onrender.com', 'untranscribable-bernita-friendless.ngrok-free.dev']
 # (development only!)
 #ALLOWED_HOSTS = ['*']  # Allow any host
 
